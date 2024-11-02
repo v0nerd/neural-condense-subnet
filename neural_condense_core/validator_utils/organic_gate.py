15c6b3636b9be229113ce60180badbc2aff6136f
toilaluan <toilaluan@github.com>
2024-11-02 08:32:49 +0000

    update

--- neural_condense_core/validator_utils/organic_gate.py
+++ neural_condense_core/validator_utils/organic_gate.py
@@ -100,11 +100,12 @@ class OrganicGate:
                 )
             target_axon = self.metagraph.axons[targeted_uid]
 
-            response: TextCompressProtocol = await self.dendrite.forward(
+            responses: TextCompressProtocol = await self.dendrite.forward(
                 axons=[target_axon],
                 synapse=synapse,
                 timeout=constants.TIER_CONFIG[request.tier].timeout,
-            )[0]
+            )
+            response = responses[0]
             compressed_tokens = np.array(response.compressed_tokens, dtype=np.float32)
             bt.logging.info(f"Compressed shape: {compressed_tokens.shape}")
             compressed_tokens = compressed_tokens.tolist()
