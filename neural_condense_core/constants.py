fbe2ebe6213db2a79f6d0d49058703fe4ad5a69c
Tran Thanh Luan <92072154+toilaluan@users.noreply.github.com>
2024-11-02 10:36:45 +0700

    Update constants.py

--- neural_condense_core/constants.py
+++ neural_condense_core/constants.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 from typing import Callable, List
 import os
 
-
 class TierConfig(BaseModel):
     incentive_percentage: float
     requests_per_epoch: int
@@ -12,12 +11,10 @@ class TierConfig(BaseModel):
     max_condensed_tokens: int
     max_context_length_in_chars: int
 
-
 class SyntheticTaskConfig(BaseModel):
     task: str
     criterias: List[str]
     rewarding_frequency: int
-
 
 class Constants(BaseModel):
     TIER_CONFIG: dict[str, TierConfig] = {
@@ -67,6 +64,7 @@ class Constants(BaseModel):
         ),
     ]
 
+    # Default values
     EPOCH_LENGTH: int = 600
     SCORING_PER_MINER_PER_EPOCH: int = 1
     SUBNET_TEMPO: int = 120
@@ -76,6 +74,15 @@ class Constants(BaseModel):
     SCORE_MOVING_AVERAGE: float = 0.05
     ORGANIC_CLIENT_URL: str = "https://ncs-client.condenses.ai"
 
+    # Adjust values based on NETWORK environment variable
+    def __init__(self, **data):
+        super().__init__(**data)
+        network = os.getenv("NETWORK")
+        if network == "test":
+            self.RPE_PERCENTAGE_FOR_SYNTHETIC = 1
+            self.EPOCH_LENGTH = 60
+            self.MIN_STAKE = 0
+            self.ORGANIC_CLIENT_URL = "https://testnet-ncs-client.condenses.ai"
 
 constants = Constants()
 
